ab4208389b4608e924d7e667aa00dfe39f07aa8b
Giapa <giapa@protonmail.com>
2020-04-15 16:08:15 +0300

    Full scrape 2 websites

--- content_feeders/news247.py
+++ content_feeders/news247.py
@@ -1,24 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-def getSummary(link):
-    #Get page response
-    response = requests.get(link)
-    #Parse the pgae
-    soup = BeautifulSoup(response.content,'html.parser')
-    #Find first paragraph
-    summary_p = soup.find('p')
-    #Get the first text
-    summary = summary_p.text[:40] + ' ...'
-    #Return the text
-    return summary
-
 def crawl_page():
     #Available urls
-    urls = ['https://www.in.gr/world/','https://www.in.gr/politics/','https://www.in.gr/economy/','https://www.in.gr/sports/','https://www.in.gr/culture/','https://www.in.gr/entertainment/','https://www.in.gr/tech/']
-    #Types of articles
-    content = ['global','politics','economy','sports','culture','entertainment','technology']
-    #Adding headers 
+    urls = ['https://www.news247.gr/politiki/','https://www.news247.gr/koinonia/','https://www.news247.gr/oikonomia/','https://www.news247.gr/kosmos/']
+    #Types of artiles
+    content = ['politics','society','economy','global']
+    #Adding headers
     headers = {
         'User-agent':'Mozilla/5.0'
     }
@@ -32,26 +20,26 @@ def crawl_page():
         response = requests.get(url, headers=headers)
         #Parse the page 
         soup = BeautifulSoup(response.content,'html.parser')
-        #Find the article element
         articles = soup.find_all('article')
         #Loop through all articles
         for article in articles:
+            if article.find('figure') is None:
+                continue
             #Title and url
-            info = article.find('a',{'href':True})
-            link = info['href']
+            info = article.find('a',{'class':'article__image'})
             title = info['title']
+            link = info['href']
             #Image
-            image_element = article.find('div',{'class':'absg'})
-            image = image_element['data-src']
+            image_element = info.find('img')
+            image = image_element['src']
             #Summary
             try:
-                sum_span = article.find('span',{'class':'article-dd'})
-                summary = sum_span.text
+                summary_p = article.find('p')
+                summary = summary_p.text
             except AttributeError:
-                summary = getSummary(link)
-            #Time
-            time_elem = article.find('time')
-            time = time_elem['datetime']
+                continue
+            time_element = article.find('time')
+            time = time_element['datetime']
             #Complete data
             article_data = {
                 'type':content[index],
