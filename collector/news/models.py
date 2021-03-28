a7b769cc909c9dcc02812e12dd90c75e6f10fd02
Giapa <giapa@protonmail.com>
2021-03-28 19:23:35 +0300

    Add addition field in news model and register to admin

--- collector/news/models.py
+++ collector/news/models.py
@@ -4,5 +4,9 @@ class News(models.Model):
     title = models.CharField(max_length=50)
     body = models.CharField(max_length=100)
     author = models.CharField(max_length=20)
+    category = models.CharField(max_length=20, default='')
     origin = models.CharField(max_length=20)
     pub_date = models.DateField('date published')
+
+    def __str__(self):
+        return self.title
